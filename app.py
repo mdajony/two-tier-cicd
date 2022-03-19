f4f13ea156943176f807e06b42dddc942e8e53d7
Shahed Mehbub <shahed.mehbub@brilliant.com.bd>
2022-03-19 14:27:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello world from CI/CD k8s 1....'
+    return 'Hello world from CI/CD k8s from gitlab-ci....'
 
 
 @app.route('/reverser',methods = ['POST'])
